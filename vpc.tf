39be44be7c1df11be0a838a5ce32a960b5192186
José <jnc.vip@gmail.com>
2024-11-06 23:17:10 -0300

    :feat:feat: Update name resource

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 # Create terra_aws_vpc VPC
-resource "aws_vpc" "tf_aws_vpc" {
+resource "aws_vpc" "terra_aws_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "Terraform_AWS_VPC"
+    Name = "Terra_AWS_VPC"
   }
 }
