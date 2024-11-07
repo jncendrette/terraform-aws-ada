a6b14f6827b30892c0ab62868577d020ae3a1fa4
José <jnc.vip@gmail.com>
2024-11-06 23:18:10 -0300

    :feat:feat: New resource subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,86 @@
+# Create tf Public Subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.terra_aws_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "Public_Subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.terra_aws_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "Public_Subnet_2"
+  }
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id                  = aws_vpc.terra_aws_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1c"
+  tags = {
+    Name = "Public_Subnet_3"
+  }
+}
+
+# Create tf Private Subnets
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.terra_aws_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Private_Subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.terra_aws_vpc.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "Private_Subnet_2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id            = aws_vpc.terra_aws_vpc.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "Private_Subnet_3"
+  }
+}
+
+# Create tf Private Database Subnets
+resource "aws_subnet" "private_database_subnet_1" {
+  vpc_id            = aws_vpc.terra_aws_vpc.id
+  cidr_block        = "10.0.7.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Private_Database_Subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_database_subnet_2" {
+  vpc_id            = aws_vpc.terra_aws_vpc.id
+  cidr_block        = "10.0.8.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "Private_Database_Subnet_2"
+  }
+}
+
+resource "aws_subnet" "private_database_subnet_3" {
+  vpc_id            = aws_vpc.terra_aws_vpc.id
+  cidr_block        = "10.0.9.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "Private_Database_Subnet_3"
+  }
+}
