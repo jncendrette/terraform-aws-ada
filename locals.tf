3d16bab71c09a07f33e735a3b544e874ffa01d62
José <jnc.vip@gmail.com>
2024-11-08 23:42:58 -0300

    sparkles:feat: Add new resources aws

--- locals.tf
+++ locals.tf
@@ -1,7 +1,3 @@
-data "aws_caller_identity" "current" {
-
-}
-
 locals {
   policy = jsonencode(
 
--- sg.tf
+++ sg.tf
@@ -3,4 +3,34 @@ resource "aws_security_group" "ec2_sg" {
   description = "Habilitar Protocolos SSH e HTTP"
   vpc_id      = aws_vpc.main_vpc.id
 
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main_vpc.cidr_block]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main_vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "EC2-SG"
+  }
 }
+
+
