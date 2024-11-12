be7456900970bdd4a8f11688668f31a41ae8d0ab
José <jnc.vip@gmail.com>
2024-11-12 00:43:01 -0300

    :feat:feat: Update file locals.tf

--- locals.tf
+++ locals.tf
@@ -12,7 +12,7 @@ locals {
             "Service" : "cloudfront.amazonaws.com"
           },
           "Action" : "s3:GetObject",
-          "Resource" : "arn:aws:s3:::/*",
+          "Resource" : "arn:aws:s3:::bucket-s3-jcendrette-414860898158/*",
           "Condition" : {
             "StringEquals" : {
               "AWS:SourceArn" : "arn:aws:cloudfront::${data.aws_caller_identity.current.id}:distribution/ELU0HE6NG0TGP"
