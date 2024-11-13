e7764372133d395183bb28c113a466a75c282526
José <jnc.vip@gmail.com>
2024-11-13 11:29:05 -0300

    :feat: Update resource rds.tf

--- rds.tf
+++ rds.tf
@@ -1,22 +1,22 @@
 # Create Cluster RDS Database  MySQL
-# resource "aws_rds_cluster" "database-mysql" {
-#   cluster_identifier        = "database-mysql"
-#   availability_zones        = var.availability_zones
-#   db_subnet_group_name      = aws_db_subnet_group.database-mysql.name
-#   engine                    = "mysql"
-#   engine_version            = "8.0.39"
-#   db_cluster_instance_class = "db.m5d.large"
-#   storage_type              = "io2"
-#   allocated_storage         = var.allocated_storage
-#   iops                      = var.iops
-#   master_username           = "admin"
-#   master_password           = var.master_password
-#   skip_final_snapshot       = true
+resource "aws_rds_cluster" "database-mysql" {
+  cluster_identifier        = "database-mysql"
+  availability_zones        = var.availability_zones
+  db_subnet_group_name      = aws_db_subnet_group.database-mysql.name
+  engine                    = "mysql"
+  engine_version            = "8.0.39"
+  db_cluster_instance_class = "db.m5d.large"
+  storage_type              = "io2"
+  allocated_storage         = var.allocated_storage
+  iops                      = var.iops
+  master_username           = "admin"
+  master_password           = var.master_password
+  skip_final_snapshot       = true
 
-#   tags = {
-#     Name = "My_RDS_MySQL"
-#   }
-# }
+  tags = {
+    Name = "My_RDS_MySQL"
+  }
+}
 
 
 
